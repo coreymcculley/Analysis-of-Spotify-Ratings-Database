1aabf6d34922b9021c187f4e523a4736d391c9e8
Christy Patrick <christy06p@msn.com>
2020-12-03 20:42:48 -0600

    starter code

---
+++ CP Work Files/app.js
@@ -0,0 +1,14 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+//schart
