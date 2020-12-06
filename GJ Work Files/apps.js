d8c47083136630ec6dfb33966a903d6af155330b
Shunaam <67766966 + Geogith@users.noreply.github.com>
2020-12-05 19:21:07 -0600

    updated code

--- GJ Work Files/apps.js
+++ GJ Work Files/apps.js
@@ -1,78 +1,89 @@
 // Load data from spotify.csv
-d3.csv("./data.df_2010.csv")
+d3.csv("df_all_data_w_decades.csv")
   .then(function (musicData) {
-    console.log(musicData);
-
-    // log a list of names
-    var names = musicData.map((data) => data.name);
-    console.log("names", names);
-
-    // Cast each name in musicData as a number using the unary + operator
-    musicData.forEach(function (data) {
-      data.names = +data.names;
-      console.log("Name:", data.name);
-      console.log("Songs:", data.songs);
+    var artistcountbydecade = {};
+    musicData.forEach((data) => {
+      if (
+        ["['Эрнест Хемингуэй']", "['Эрих Мария Ремарк']"].includes(data.artists)
+      ) {
+      }
+      if (!artistcountbydecade[data.decade]) {
+        artistcountbydecade[data.decade] = {};
+      }
+      if (artistcountbydecade[data.decade][data.artists]) {
+        artistcountbydecade[data.decade][data.artists] += 1;
+      } else {
+        artistcountbydecade[data.decade][data.artists] = 1;
+      }
     });
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
 
-// Define SVG area dimensions
-var svgWidth = 960;
-var svgHeight = 660;
+    var toptenartistsbydecade = {};
 
-// Define the chart's margins as an object
-var chartMargin = {
-  top: 30,
-  right: 30,
-  bottom: 30,
-  left: 30,
-};
-
-// Define dimensions of the chart area
-var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
-
-// Select body, append SVG area to it, and set the dimensions
-var svg = d3
-  .select("body")
-  .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
-
-// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
-var chartGroup = svg
-  .append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
-
-// Load data from df_2010.csv
-d3.csv("data.df_2010.csv")
-  .then(function (musicData) {
-    console.log(musicData);
-
-    // Cast the names to a number for each piece of musicData
-    tvData.forEach(function (d) {
-      d.names = +d.names;
+    _.forEach(artistcountbydecade, (value, key) => {
+      var toptenartists = [];
+      for (var i = 0; i < 5; i++) {
+        var currentmax = Math.max(...Object.values(value));
+        Object.keys(value).forEach((artists) => {
+          if (value[artists] == currentmax) {
+            toptenartists.push(artists);
+            value[artists] = -1;
+          }
+        });
+      }
+      toptenartistsbydecade[key] = toptenartists.slice(0, 5);
     });
+
+    delete toptenartistsbydecade[2020];
+
+    console.log(artistcountbydecade);
+    console.log(toptenartistsbydecade);
+
+    // Define SVG area dimensions
+    var svgWidth = 1060;
+    var svgHeight = 860;
+
+    // Define the chart's margins as an object
+    var chartMargin = {
+      top: 30,
+      right: 30,
+      bottom: 30,
+      left: 350,
+    };
+
+    // Define dimensions of the chart area
+    var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+    var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+    console.log(chartWidth, chartHeight);
+
+    // Select body, append SVG area to it, and set the dimensions
+    var svg = d3
+      .select("body")
+      .append("svg")
+      .attr("height", svgHeight)
+      .attr("width", svgWidth);
+
+    // Append a group to the SVG area and shift ('translate') it to the right and to the bottom
+    var chartGroup = svg
+      .append("g")
+      .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
     // Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
-    var xBandScale = d3
-      .scaleBand()
-      .domain(musicData.map((d) => d.songs))
-      .range([0, chartWidth])
-      .padding(0.1);
+    var xscale = d3.scaleLinear().domain([1920, 2020]).range([0, chartWidth]);
+    var allartists = [];
+    Object.values(toptenartistsbydecade).forEach((topartists) => {
+      allartists = allartists.concat(topartists);
+    });
+
+    allartists = _.uniq(allartists);
 
     // Create a linear scale for the vertical axis.
-    var yLinearScale = d3
-      .scaleLinear()
-      .domain([0, d3.max(musicData, (d) => d.names)])
-      .range([chartHeight, 0]);
+    var yscale = d3.scaleBand().domain(allartists).range([chartHeight, 0]);
 
     // Create two new functions passing our scales in as arguments
     // These will be used to create the chart's axes
-    var bottomAxis = d3.axisBottom(xBandScale);
-    var leftAxis = d3.axisLeft(yLinearScale).ticks(10);
+    var bottomAxis = d3.axisBottom(xscale);
+    var leftAxis = d3.axisLeft(yscale).ticks(10);
 
     // Append two SVG group elements to the chartGroup area,
     // and create the bottom and left axes inside of them
@@ -87,14 +98,49 @@ d3.csv("data.df_2010.csv")
     // Use the linear and band scales to position each rectangle within the chart
     chartGroup
       .selectAll(".bar")
-      .data(tvData)
+      .data(allartists)
       .enter()
       .append("rect")
       .attr("class", "bar")
-      .attr("x", (d) => xBandScale(d.songs))
-      .attr("y", (d) => yLinearScale(d.names))
-      .attr("width", xBandScale.bandwidth())
-      .attr("height", (d) => chartHeight - yLinearScale(d.names));
+      .attr("x", (d) => {
+        var minartistdecade = 2030;
+        Object.keys(toptenartistsbydecade).forEach((decade) => {
+          if (
+            toptenartistsbydecade[decade].find((artists) => {
+              return artists == d;
+            })
+          ) {
+            if (Number(decade) < minartistdecade) {
+              minartistdecade = Number(decade);
+            }
+          }
+        });
+        return xscale(Number(minartistdecade));
+      })
+
+      .attr("y", (d) => yscale(d))
+      .attr("width", (d) => {
+        var maxartistdecade = 0;
+        var minartistdecade = 2030;
+        Object.keys(toptenartistsbydecade).forEach((decade) => {
+          if (
+            toptenartistsbydecade[decade].find((artists) => {
+              return artists == d;
+            })
+          ) {
+            if (Number(decade) > maxartistdecade) {
+              maxartistdecade = Number(decade);
+            }
+            if (Number(decade) < minartistdecade) {
+              minartistdecade = Number(decade);
+            }
+          }
+        });
+        return (
+          xscale(Number(maxartistdecade) + 10) - xscale(Number(minartistdecade))
+        );
+      })
+      .attr("height", 20);
   })
   .catch(function (error) {
     console.log(error);
