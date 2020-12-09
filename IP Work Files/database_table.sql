0c005a51e0c6aacf6b05e4a345d17654d1b50dc0
ipavlunin <ipavlunin@gmail.com>
2020-12-09 10:50:10 -0600

    clean data and PostgresDB

---
+++ IP Work Files/database_table.sql
@@ -0,0 +1,31 @@
+-- DROP TABLE IF EXISTS spotify_songs;
+
+CREATE TABLE spotify_songs
+(
+    -- 	Columns created for cleaned database
+    Decade INT,
+    Year INT,
+    Artist VARCHAR(500),
+    Song VARCHAR(500),
+    Genre VARCHAR(1000),
+    Duration_ms INT,
+    ArtistSong VARCHAR(1000),
+    Valence NUMERIC,
+    Acousticness NUMERIC,
+    Danceability NUMERIC,
+    Energy NUMERIC,
+    Explicit INT,
+    Instrumentalness NUMERIC,
+    Key INT,
+	Liveness NUMERIC,
+	Loudness NUMERIC,
+	Mode INT,
+	Popularity INT,
+	Speechiness NUMERIC,
+	Tempo NUMERIC,
+	ID VARCHAR
+    (30) PRIMARY KEY
+);
+
+    select *
+    from spotify_songs
