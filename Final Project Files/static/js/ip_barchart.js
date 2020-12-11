f4516ed3c1e191b669c2fa47684418ffd46a43b4
coreymcculley <corey.mcculley@gmail.com>
2020-12-11 09:30:33 -0600

    Latest HTML files

--- Final Project Files/static/js/ip_barchart.js
+++ Final Project Files/static/js/ip_barchart.js
@@ -1,5 +1,5 @@
 function updateBar(band){
-    d3.csv("clean_data_all.csv", function (getData) {
+    d3.csv("../data/clean_data_all.csv", function (getData) {
 
         var songData = [];
         songData = getData;
